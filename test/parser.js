c0938c4d570e1c1f7748958fff8085d327e68e65
Jason Frame <jason@onehackoranother.com>
2014-08-29 20:46:17 +0100

    ast generation

--- test/parser.js
+++ test/parser.js
@@ -1,4 +1,6 @@
 var parser = require('../lib/parser');
 var source = require('fs').readFileSync(__dirname + '/test1.tmptn', 'utf8');
 
-parser.parse(source);
+var res = parser.parse(source);
+
+console.log(require('util').inspect(res, {depth: null, colors: true}));
