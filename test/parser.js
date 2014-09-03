1b81d23e32fcb8778e3b61c21d91c2dc77f87c3e
Jason Frame <jason@onehackoranother.com>
2014-09-03 01:45:16 +0100

    i have no idea what i’m doing

--- test/parser.js
+++ test/parser.js
@@ -3,4 +3,6 @@ var source = require('fs').readFileSync(__dirname + '/test1.tmptn', 'utf8');
 
 var res = parser.parse(source);
 
-console.log(require('util').inspect(res, {depth: null, colors: true}));
+var js = require('../lib/compiler')(res);
+
+console.log(js);
